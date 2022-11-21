2719c6e2dbab6923e22c54a8287c4fc11a8e3bf8
fzuch <358806762@qq.com>
2022-11-22 00:29:41 +0800

    十一月二十一日提交初始数据仓库

---
+++ houtaiguanli/chenhe/learn_mybatis/src/mybatis_test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class mybatis_test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println("ababab");
+            case 2:
+                System.out.println("不认同");
+            case 3:
+                System.out.println("软工早点死");
+        }
+    }
+}
